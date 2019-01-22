8cf9b2bb75e60f2875423bc4b8c6d098a6ead92b
drakessn <drakessn@gmail.com>
2019-01-22 07:44:20 -0500

    Predicting stock prices.

--- reco.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from lightfm.datasets import fetch_movielens
-from lightfm import LightFM
-
-data = fetch_movielens(min_rating=4.0)
-
-print(repr(data['train']))
-print(repr(data['test']))
